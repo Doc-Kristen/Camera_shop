4226384e0a1b804a622931828e18bee419d0d0de
unknown <kristinamisenko9@gmail.com>
2022-10-26 02:44:39 +0300

    написала тесты для редьюсера userProcess

--- src/store/user-process/user-process.test.ts
+++ src/store/user-process/user-process.test.ts
@@ -1,3 +1,5 @@
+import { setModalOpeningStatus, setReviewErrorStatus, setSuccessOpeningStatus } from '../action';
+import { sendReview } from '../api-actions';
 import { userProcess } from './user-process';
 
 
@@ -13,4 +15,111 @@ describe('Reducer: productData', () => {
       });
   });
 
+  it('should update state when sended review', () => {
+    const state = {
+      isFormOpened: false,
+      isFormBlocked: false,
+      isReviewPosted: false,
+      isErrorSendingReview: false,
+      isReviewSuccess: false,
+    };
+    expect(userProcess.reducer(state, { type: sendReview.fulfilled.type}))
+      .toEqual({
+        isFormOpened: false,
+        isFormBlocked: false,
+        isReviewPosted: false,
+        isErrorSendingReview: false,
+        isReviewSuccess: true,
+      });
+  });
+
+  it('should update isFormBlocked, isReviewPosted  when sendReview.pending', () => {
+    const state = {
+      isFormOpened: true,
+      isFormBlocked: false,
+      isReviewPosted: false,
+      isErrorSendingReview: false,
+      isReviewSuccess: false,
+    };
+    expect(userProcess.reducer(state, { type: sendReview.pending.type}))
+      .toEqual({
+        isFormOpened: true,
+        isFormBlocked: true,
+        isReviewPosted: true,
+        isErrorSendingReview: false,
+        isReviewSuccess: false,
+      });
+  });
+
+  it('should update isFormBlocked, isReviewPosted, isErrorSendingReview  when sendReview.rejected', () => {
+    const state = {
+      isFormOpened: false,
+      isFormBlocked: true,
+      isReviewPosted: true,
+      isErrorSendingReview: false,
+      isReviewSuccess: false,
+    };
+    expect(userProcess.reducer(state, { type: sendReview.rejected.type}))
+      .toEqual({
+        isFormOpened: false,
+        isFormBlocked: false,
+        isReviewPosted: false,
+        isErrorSendingReview: true,
+        isReviewSuccess: false,
+      });
+  });
+
+  it('should update isFormOpened when dispatch setModalOpeningStatus', () => {
+    const state = {
+      isFormOpened: false,
+      isFormBlocked: false,
+      isReviewPosted: false,
+      isErrorSendingReview: false,
+      isReviewSuccess: false,
+    };
+    expect(userProcess.reducer(state, setModalOpeningStatus(true)))
+      .toEqual({
+        isFormOpened: true,
+        isFormBlocked: false,
+        isReviewPosted: false,
+        isErrorSendingReview: false,
+        isReviewSuccess: false,
+      });
+  });
+
+  it('should update isErrorSendingReview when dispatch setReviewErrorStatus', () => {
+    const state = {
+      isFormOpened: false,
+      isFormBlocked: false,
+      isReviewPosted: false,
+      isErrorSendingReview: false,
+      isReviewSuccess: false,
+    };
+    expect(userProcess.reducer(state, setReviewErrorStatus(true)))
+      .toEqual({
+        isFormOpened: false,
+        isFormBlocked: false,
+        isReviewPosted: false,
+        isErrorSendingReview: true,
+        isReviewSuccess: false,
+      });
+  });
+
+  it('should update isReviewSuccess when dispatch setSuccessOpeningStatus', () => {
+    const state = {
+      isFormOpened: false,
+      isFormBlocked: false,
+      isReviewPosted: false,
+      isErrorSendingReview: false,
+      isReviewSuccess: false,
+    };
+    expect(userProcess.reducer(state, setSuccessOpeningStatus(true)))
+      .toEqual({
+        isFormOpened: false,
+        isFormBlocked: false,
+        isReviewPosted: false,
+        isErrorSendingReview: false,
+        isReviewSuccess: true,
+      });
+  });
 });
